cc6e3e3bf6afa2b29a2cb656641e1d4c8c9c3ef6
Valery Baiburin <valery@ortech.co.jp>
2019-03-01 13:17:31 +0900

    Refactor Module3.Task11

--- Module3/Task11.hs
+++ Module3/Task11.hs
@@ -5,5 +5,5 @@ perms [] = [[]]
 perms [x] = [[x]]
 perms (x : xs) = concatMap (insert [] x) (perms xs)
 
-insert ls x rhs@(r : rs) = (ls ++ [x] ++ rhs) : insert (ls ++ [r]) x rs
 insert ls x [] = [ls ++ [x]]
+insert ls x rhs@(r : rs) = (ls ++ [x] ++ rhs) : insert (ls ++ [r]) x rs
