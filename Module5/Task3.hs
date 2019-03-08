257a60cef95f7aac303c88358ede53991928ea5e
Valery Baiburin <valery@ortech.co.jp>
2019-03-08 15:51:59 +0900

    Refactor Module5.Task3

--- Module5/Task3.hs
+++ Module5/Task3.hs
@@ -3,6 +3,5 @@ module Module5.Task3 where
 -- data Tree a = Leaf (Maybe a) | Branch (Tree a) (Maybe a) (Tree a) deriving Show
 
 instance Functor Tree where
-    fmap _ (Leaf Nothing) = Leaf Nothing
-    fmap f (Leaf (Just x)) = Leaf (Just (f x))
+    fmap f (Leaf x) = Leaf (fmap f x)
     fmap f (Branch l x r) = Branch (fmap f l) (fmap f x) (fmap f r)
